52e3597781cf9f196203abcfef484212793aa138
Celina Sophie Kalus <hello@celinakalus.de>
2024-08-09 18:17:23 +0200

    Choose queue and create logical device`
    Signed-off-by: Celina Sophie Kalus <hello@celinakalus.de>

--- src/vkwnd.c
+++ src/vkwnd.c
@@ -1,3 +1,5 @@
+#include "defines.h"
+#include <stdint.h>
 #include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -95,7 +97,7 @@ SB_RESULT vkwnd_create(VulkanWindow *vkwnd, const char *appName, int major, int 
 		return SB_EXIT_FAILURE;
 	}
 
-	for (int device_i = 0; device_i < device_count; device_i++) {
+	for (int device_i = 0; device_i < (long)device_count; device_i++) {
 		VkPhysicalDevice device = phys_devices[device_i];
 		VkPhysicalDeviceProperties device_prop;
 		vkGetPhysicalDeviceProperties(device, &device_prop);
@@ -104,16 +106,66 @@ SB_RESULT vkwnd_create(VulkanWindow *vkwnd, const char *appName, int major, int 
 
 	printf("Choosing first device.\n");
 
-	VkPhysicalDevice phys_device = phys_devices[0];
+	vkwnd->phys_device = phys_devices[0];
 	free(phys_devices);
 	(void)phys_devices;
 
+	VkQueueFamilyProperties *queue_prop;
+	uint32_t queue_count = 0;
+
+	vkGetPhysicalDeviceQueueFamilyProperties(vkwnd->phys_device, &queue_count, NULL);
+
+	queue_prop = malloc(sizeof(VkQueueFamilyProperties) * queue_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(vkwnd->phys_device, &queue_count, queue_prop);
+
+	int queue_chosen = -1;
+	for (int i = 0; i < (long)queue_count; i++) {
+		if (queue_prop[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			queue_chosen = i;
+			break;
+		}
+	}
+
+	if (queue_chosen < 0) {
+		printf("No suitable queue found.\n");
+		return SB_EXIT_FAILURE;
+	}
+
+	printf("Graphics queue %i chosen.\n", queue_chosen);
+
+	VkDeviceQueueCreateInfo queue_create_info = { 0, };
+	queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queue_create_info.queueFamilyIndex = queue_chosen;
+	queue_create_info.queueCount = 1;
+
+	float queue_priority = 1.0f;
+	queue_create_info.pQueuePriorities = &queue_priority;
+
+	VkPhysicalDeviceFeatures device_features = { 0, };
+
+	VkDeviceCreateInfo log_dev_create_info = { 0, };
+	log_dev_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	log_dev_create_info.pQueueCreateInfos = &queue_create_info;
+	log_dev_create_info.queueCreateInfoCount = 1;
+	log_dev_create_info.pEnabledFeatures = &device_features;
+	log_dev_create_info.enabledExtensionCount = 0;
+	log_dev_create_info.enabledLayerCount = 0;
+
+	result = vkCreateDevice(vkwnd->phys_device, &log_dev_create_info, NULL, &(vkwnd->log_device));
+
+	if (result != VK_SUCCESS) {
+		printf("Failed to create logical device: %i\n", result);
+	}
+
 	printf("Vulkan window created successfully.\n");
+
+	vkGetDeviceQueue(vkwnd->log_device, queue_chosen, 0, &(vkwnd->graphics_queue));
 
 	return SB_EXIT_SUCCESS;
 }
 
 void vkwnd_destroy(VulkanWindow *vkwnd) {
+	vkDestroyDevice(vkwnd->log_device, NULL);
 	vkDestroyInstance(vkwnd->instance, NULL);
 	glfwDestroyWindow(vkwnd->window);
 	glfwTerminate();
--- src/vkwnd.h
+++ src/vkwnd.h
@@ -1,6 +1,7 @@
 #ifndef __WINDOW_H
 #define __WINDOW_H
 
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include "defines.h"
@@ -8,6 +9,9 @@
 typedef struct {
 	VkInstance instance;
 	GLFWwindow *window;
+	VkPhysicalDevice phys_device;
+	VkDevice log_device;
+	VkQueue graphics_queue;
 } VulkanWindow;
 
 SB_RESULT vkwnd_create(VulkanWindow *vkwnd, const char *appName, int major, int minor, int build, int width, int height);
